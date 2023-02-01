5fef817d217ad9e5487683316dad986f6c96ea1b
Khurram Khalil <kkhalil.pg@smme.edu.pk>
2023-02-01 12:34:09 +0500

    Added networkx stuff.

---
+++ mht_lectures/netx/infere_straight_path.py
@@ -0,0 +1,55 @@
+import numpy as np
+import plotly.graph_objects as go
+import networkx as nx
+import math
+
+
+def x_align(length, offset):
+    x_vals = np.linspace(0, 10, length)
+    y_vals = np.repeat(offset, length)
+
+    reading = np.vstack((x_vals, y_vals))
+    reading = reading.T
+
+    return reading
+
+
+if __name__ == "__main__":
+    duration = 5
+    dims = 2
+    num_targets = 2
+
+    G = nx.Graph()
+
+    target_0 = x_align(duration, 2)
+    target_1 = x_align(duration, 5)
+
+    for time_, j in enumerate(zip(target_0, target_1)):
+        if time_ == 0:
+            G.add_node(0, weight=1, loc=j[0].tolist())
+            G.add_node(1, weight=1, loc=j[1].tolist())
+            # G.add_nodes_from(range(num_targets))
+
+        else:
+            # First detected target
+            G.add_node(len(G.nodes), weight=1, loc=j[0])
+            diff_0 = math.dist(j[0], G.nodes[len(G.nodes) - 3]['loc'])
+            diff_1 = math.dist(j[0], G.nodes[len(G.nodes) - 2]['loc'])
+
+            if diff_0 < diff_1:
+                G.add_edge(len(G.nodes) - 1, len(G.nodes) - 3)
+            else:
+                G.add_edge(len(G.nodes) - 1, len(G.nodes) - 2)
+
+            # Second detected target
+            G.add_node(len(G.nodes), weight=1, loc=j[1])
+            diff_0 = math.dist(j[1], G.nodes[len(G.nodes) - 3]['loc'])
+            diff_1 = math.dist(j[1], G.nodes[len(G.nodes) - 2]['loc'])
+
+            if diff_0 < diff_1:
+                G.add_edge(len(G.nodes) - 1, len(G.nodes) - 3)
+            else:
+                G.add_edge(len(G.nodes) - 1, len(G.nodes) - 2)
+
+        print(time_, j)
+
---
+++ mht_lectures/netx/intro_networkx.py
@@ -0,0 +1,40 @@
+import networkx as nx
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+# Create an empty graph
+G = nx.Graph()
+
+# Add nodes to the graph
+G.add_node(1)
+G.add_node(2)
+G.add_node(3)
+
+# Add edges to the graph
+G.add_edge(1, 2)
+G.add_edge(2, 3)
+
+# Check number of nodes and edges in the graph
+print("Number of nodes:", G.number_of_nodes())
+print("Number of edges:", G.number_of_edges())
+
+# Get a list of all nodes in the graph
+print("Nodes:", G.nodes())
+
+# Get a list of all edges in the graph
+print("Edges:", G.edges())
+
+# Check if node 4 is in the graph
+print("Node 4 in graph:", 4 in G)
+
+# Remove node 3 from the graph
+G.remove_node(3)
+
+# Check number of nodes and edges in the graph after removing node 3
+print("Number of nodes:", G.number_of_nodes())
+print("Number of edges:", G.number_of_edges())
+
+# Draw the graph
+plt.figure()
+nx.draw(G)
+plt.show()
