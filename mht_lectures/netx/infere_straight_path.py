77cc19fc9c3062c5183c54d46e49b3c479cea52a
Khurram Khalil <kkhalil.pg@smme.edu.pk>
2023-02-06 11:56:01 +0500

    New updates.

--- mht_lectures/netx/infere_straight_path.py
+++ mht_lectures/netx/infere_straight_path.py
@@ -1,9 +1,11 @@
+# Import libraies
 import math
 
 import networkx as nx
 import numpy as np
 
 
+# Generate crossing path
 def x_align(length, offset):
     x_vals = np.linspace(0, 10, length)
     y_vals = np.repeat(offset, length)
@@ -19,11 +21,14 @@ if __name__ == "__main__":
     dims = 2
     num_targets = 2
 
+    # Initialize graph object
     G = nx.Graph()
 
+    # As targets are known, we initialize two local hypothesis
     target_0 = x_align(duration, 2)
     target_1 = x_align(duration, 5)
 
+    # Now, iterate through the detections and update local hypothesis based on shortest distance
     for time_, j in enumerate(zip(target_0, target_1)):
         if time_ == 0:
             G.add_node(0, weight=1, loc=j[0].tolist())
@@ -51,4 +56,5 @@ if __name__ == "__main__":
             else:
                 G.add_edge(len(G.nodes) - 1, len(G.nodes) - 2)
 
-        print(time_, j)
+    nx.draw(G)
+    print(time_, j)
